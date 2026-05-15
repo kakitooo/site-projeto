f78c8eb7cd16819c79853c87e9eaf9c7508fa018
Alinequintals <aline.lima293@etec.sp.gov.br>
2026-05-15 10:02:38 -0300

    Organiza menus e adiciona páginas do projeto

---
+++ scripts/login.js
@@ -0,0 +1,27 @@
+const form = document.querySelector(".form-login");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const senha = document.getElementById("senha").value;
+
+    // LOGIN FICTÍCIO
+    const emailCorreto = "teosantos@gmail.com";
+    const senhaCorreta = "123456";
+
+    if(email === emailCorreto && senha === senhaCorreta) {
+
+        alert("Login realizado com sucesso!");
+
+        // REDIRECIONA
+        window.location.href = "perfil.html";
+
+    } else {
+
+        alert("E-mail ou senha incorretos.");
+
+    }
+
+});
